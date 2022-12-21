ad596ed3134d1818e026b5b27f0c2b0aacc0f41e
Neutx <dlprwz@gmail.com>
2022-12-22 04:22:51 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import './quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -25,6 +29,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper= [  ];
+  int questionNumber = 0;
+
+  List<bool> answers =[
+    false,
+    true,
+    true,
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +52,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -50,18 +65,33 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
               child: Text(
                 'True',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20.0,
                 ),
+
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
+
+                if (correctAnswer==true){
+                  print('user got it right');
+                } else{
+                  print('user got it wrong');
+                }
+
+                questionNumber++ ;
+                setState(() {
+                  scoreKeeper.add(
+                      Icon(Icons.check, color: Colors.green,)
+                  ) ;
+                });
               },
             ),
           ),
@@ -69,8 +99,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red ),
               child: Text(
                 'False',
                 style: TextStyle(
@@ -80,15 +110,32 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
+                if (correctAnswer==true){
+                  print('user got it rig ht');
+                } else{
+                  print('user got it wrong');
+                }
+
+                questionNumber++;
+                setState(() {
+                  scoreKeeper.add(
+                      Icon(Icons.check, color: Colors.red,)
+                  ) ;
+                });
+
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
+
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
